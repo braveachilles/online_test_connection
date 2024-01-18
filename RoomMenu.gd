cf069de6aad35875c9e58ca2e28bcf2d20a6a2a1
braveachilles <96799494+braveachilles@users.noreply.github.com>
2024-01-18 20:46:44 -0300

    init online mode

---
+++ RoomMenu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Here must be created the connection (network multiplayer ppeer)
+
+
+func _on_CreateRoom_pressed():
+	pass # Replace with function body.
